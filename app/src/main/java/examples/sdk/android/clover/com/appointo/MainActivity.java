78cc514c1bfd7003c472c05013ccb398a3fd1473
Vardan Gupta <vardan.gupta@as-MacBook-Pro.local>
2019-02-20 12:20:39 -0800

    Adding Changes for Splash Screen

--- app/src/main/java/examples/sdk/android/clover/com/appointo/MainActivity.java
+++ app/src/main/java/examples/sdk/android/clover/com/appointo/MainActivity.java
@@ -1,5 +1,6 @@
 package examples.sdk.android.clover.com.appointo;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
   }
 }
---
+++ app/src/main/java/examples/sdk/android/clover/com/appointo/SplashScreen.java
@@ -0,0 +1,82 @@
+package examples.sdk.android.clover.com.appointo;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashScreen extends AppCompatActivity {
+
+
+  private View mContentView;
+  Intent intent;
+
+  private final Runnable mShowPart2Runnable = new Runnable() {
+    @Override
+    public void run() {
+      // Delayed display of UI elements
+      ActionBar actionBar = getSupportActionBar();
+      if (actionBar != null) {
+        actionBar.show();
+      }
+    }
+  };
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_splash_screen);
+    intent = new Intent(this, MainActivity.class);
+
+    animate();
+    new AsyncTask<String, String, Integer>() {
+      @SuppressLint("WrongThread")
+      @Override
+      protected Integer doInBackground(String... strings) {
+        try {
+          Thread.sleep(10000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+
+        return 3;
+      }
+
+      @Override
+      protected void onPostExecute(Integer aVoid) {
+        startActivity(intent);
+        finish();
+      }
+    }.execute();
+
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+  }
+
+  public void animate() {
+    ImageView img = (ImageView)findViewById(R.id.imgview);
+    Animation aniFade = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_out);
+    img.startAnimation(aniFade);
+  }
+
+}
