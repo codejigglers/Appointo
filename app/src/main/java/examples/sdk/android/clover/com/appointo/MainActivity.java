aaa6ebd6981ebc49653a770aaac9a975bf3bb836
Vardan Gupta <vardan.gupta@as-MacBook-Pro.local>
2019-02-22 10:26:25 -0800

    Added RecyclerViews for appointment

--- app/src/main/java/examples/sdk/android/clover/com/appointo/MainActivity.java
+++ app/src/main/java/examples/sdk/android/clover/com/appointo/MainActivity.java
@@ -6,11 +6,16 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
 
@@ -21,6 +26,10 @@ public class MainActivity extends BaseActivity {
   protected ImageView profile;
   private View navHeader;
   private NavigationView navigationView;
+  private ArrayList<AppointmentModel> data = new ArrayList<>();
+  private LinearLayout noAppointmentLayout;
+  private LinearLayout appointmentLayout;
+  private RecyclerView appointmentRv;
 
   @Override
   public int getLayoutResourceId() {
@@ -31,6 +40,8 @@ public class MainActivity extends BaseActivity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    noAppointmentLayout = findViewById(R.id.no_appointment_view);
+    appointmentLayout = findViewById(R.id.appointment_view);
     navigationView = (NavigationView) findViewById(R.id.nav_view);
     navHeader = navigationView.getHeaderView(0);
     profile = (ImageView) navHeader.findViewById(R.id.img_header_bg);
@@ -38,6 +49,7 @@ public class MainActivity extends BaseActivity {
     drawerLayout.getBackground().setAlpha(127);
     loadingFragment = LoadingFragment.newInstance();
     fm = findViewById(R.id.loader);
+    appointmentRv = findViewById(R.id.appointment_recyclerview);
     ft = getSupportFragmentManager().beginTransaction();
     ft.add(R.id.loader, loadingFragment, "MyLoader").commit();
 
@@ -48,12 +60,37 @@ public class MainActivity extends BaseActivity {
         .diskCacheStrategy(DiskCacheStrategy.ALL)
         .into(profile);
 
+    fetchData();
+  }
+
+  public void removeLoader() {
+    getSupportFragmentManager().beginTransaction().remove(loadingFragment).commitAllowingStateLoss();
+    fm.setVisibility(View.GONE);
+    drawerLayout.getBackground().setAlpha(0);
+  }
+
+  public void setupUI() {
+    removeLoader();
+    if (data.size() > 0) {
+      noAppointmentLayout.setVisibility(View.GONE);
+      appointmentLayout.setVisibility(View.VISIBLE);
+      AppointmentAdapter appointmentAdapter = new AppointmentAdapter(data);
+      appointmentRv.setLayoutManager(new LinearLayoutManager(this));
+      appointmentRv.setAdapter(appointmentAdapter);
+    } else {
+      appointmentLayout.setVisibility(View.GONE);
+      noAppointmentLayout.setVisibility(View.VISIBLE);
+    }
+
+  }
+
+  public void fetchData() {
     new AsyncTask<Void, Void, Integer>() {
 
       @Override
       protected Integer doInBackground(Void... voids) {
         try {
-          Thread.sleep(1000);
+          Thread.sleep(5000);
         } catch (InterruptedException e) {
           e.printStackTrace();
         }
@@ -63,17 +100,38 @@ public class MainActivity extends BaseActivity {
       @Override
       protected void onPostExecute(Integer integer) {
         super.onPostExecute(integer);
-        removeLoader();
+        setupUI();
       }
     }.execute();
 
-  }
+    AppointmentModel dataItem = new AppointmentModel();
+    dataItem.date = "26, Jan, 2016";
+    dataItem.time = "11:00 AM - 2:00 PM";
+    dataItem.name = "Prof. Eskafi";
 
-  public void removeLoader() {
-    getSupportFragmentManager().beginTransaction().remove(loadingFragment).commitAllowingStateLoss();
-    fm.setVisibility(View.GONE);
-    drawerLayout.getBackground().setAlpha(0);
-  }
+    data.add(dataItem);
 
+    AppointmentModel dataItem2 = new AppointmentModel();
+    dataItem2.date = "26, Feb, 2016";
+    dataItem2.time = "1:00 AM - 2:00 PM";
+    dataItem2.name = "Prof. Moataghed";
+
+    data.add(dataItem2);
+
+    AppointmentModel dataItem3 = new AppointmentModel();
+    dataItem3.date = "26, Feb, 2016";
+    dataItem3.time = "1:00 AM - 2:00 PM";
+    dataItem3.name = "Prof. Moataghed";
+
+    data.add(dataItem3);
+
+    AppointmentModel dataItem4 = new AppointmentModel();
+    dataItem4.date = "26, Feb, 2016";
+    dataItem4.time = "1:00 AM - 2:00 PM";
+    dataItem4.name = "Prof. Moataghed";
+
+    data.add(dataItem4);
+    data.clear();
+  }
 
 }
